a2448a5f101ea80e51226423fd6e71726f71100a
ARAY! <charlesmatthewong2019@gmail.com>
2025-01-30 11:53:31 +0800

    Added two constructors: fractions and rational

--- Fraction.py
+++ Fraction.py
@@ -1,7 +1,21 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        #TODO
+        #TODO constructors
+
+        # Pair of integers
+        def __init__(self, intA, intB):
+            self.numerator = intA
+            self.denominator = intB
+
+        # Rational number
+        def __init__(self, rational):
+            
+            self.numerator = rational
+            self.denominator = 1
+
+
+        # String
         pass
 
     def gcd(a, b):
