385ef635acee988197e850dc64fe3d9f0a86ab4a
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-01-30 23:37:49 +0800

    Cleaned the constructors
    - Added more checks when the input is a string:
    
    - Checks the list if there are only 2 items.
    
    - Added temporary variables  for numerator and denominator that will hold the variables that haven't been checked if it is a digit
    
    - Added .strip('-') so it can handle negative signs
    
    - Added another check for the Denominator being 0 when the input is a String

--- Fraction.py
+++ Fraction.py
@@ -1,29 +1,42 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        #TODO constructors
         if denominator == 0:
             raise ZeroDivisionError('Denominator cannot be zero.')
         
-         #check if it is a string ('5/7') if not, keep checking if the inputs are integers
+        #check if it is a string ('5/7') if not, keep checking if the inputs are integers
         if  isinstance(numerator,str):
             splitNumberList = numerator.strip().split('/')
 
-            tempNumerator = int(splitNumberList[0])
-            tempDenominator = int(splitNumberList[1])
+            # There should only be 2 items in the list
+            if len(splitNumberList) != 2:
+                raise ValueError('Invalid fraction format')
             
-            if isinstance(tempNumerator,int) and isinstance(tempDenominator,int):
-                self.numerator = tempNumerator
-                self.denominator = tempDenominator
+            tempNumerator = splitNumberList[0]
+            tempDenominator = splitNumberList[1]
+            
+            # Strip the items of the signs
+            if tempNumerator.strip('-').isdigit() and tempDenominator.strip('-').isdigit():
+                # still a temp variable but atleast it has been checked as a digit
+                digitNumerator = int(tempNumerator)
+                digitDenominator = int(tempDenominator)
+
+                if digitDenominator == 0:
+                    raise ZeroDivisionError('Denominator cannot be zero.')
+                
+                if isinstance(digitNumerator,int) and isinstance(digitDenominator,int):
+                    self.numerator = digitNumerator
+                    self.denominator = digitDenominator
             else:
                 raise ValueError('Numerator and Denominator must be digits')
             
         elif not isinstance(numerator,int) or not isinstance(denominator,int):
-            raise ValueError('Numerator and Denominator must be Integers.') 
+                raise ValueError('Numerator and Denominator must be Integers.')            
         else:
+            # Handles pair of integers Fraction(5,7) and rational number Fraction 10
             self.numerator = numerator
             self.denominator = denominator
-
+                
     def gcd(a, b):
         #TODO
         pass
