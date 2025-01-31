791e4a75b7937ed81d2dbe4030c06ba72245eca9
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-01-31 19:48:40 +0800

    Changed the Error Raises
    I realized it was supposed to output the default 0/1 numerator/denominator if the input is invalid

--- Fraction.py
+++ Fraction.py
@@ -10,28 +10,28 @@ class Fraction(object):
 
             # There should only be 2 items in the list
             if len(splitNumberList) != 2:
-                raise ValueError('Invalid fraction format')
-            
-            tempNumerator = splitNumberList[0]
-            tempDenominator = splitNumberList[1]
-            
-            # Strip the items of the signs
-            if tempNumerator.strip('-').isdigit() and tempDenominator.strip('-').isdigit():
-                # still a temp variable but atleast it has been checked as a digit
-                digitNumerator = int(tempNumerator)
-                digitDenominator = int(tempDenominator)
-
-                if digitDenominator == 0:
-                    raise ZeroDivisionError('Denominator cannot be zero.')
-                
-                if isinstance(digitNumerator,int) and isinstance(digitDenominator,int):
-                    self.numerator = digitNumerator
-                    self.denominator = digitDenominator
+                self.numerator = 0
+                self.denominator = 1
             else:
-                raise ValueError('Numerator and Denominator must be digits')
+                tempNumerator = splitNumberList[0]
+                tempDenominator = splitNumberList[1]
+            
+                # Strip the items of the signs
+                if tempNumerator.strip('-').isdigit() and tempDenominator.strip('-').isdigit():
+                    # still a temp variable but atleast it has been checked as a digit
+                    digitNumerator = int(tempNumerator)
+                    digitDenominator = int(tempDenominator)
+
+                    if digitDenominator == 0:
+                        raise ZeroDivisionError('Denominator cannot be zero.')
+                    
+                    if isinstance(digitNumerator,int) and isinstance(digitDenominator,int):
+                        self.numerator = digitNumerator
+                        self.denominator = digitDenominator
             
         elif not isinstance(numerator,int) or not isinstance(denominator,int):
-                raise ValueError('Numerator and Denominator must be Integers.')            
+                self.numerator = 0
+                self.denominator = 1            
         else:
             # Handles pair of integers Fraction(5,7) and rational number Fraction 10
             self.numerator = numerator
